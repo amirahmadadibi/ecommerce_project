675f1e03c0a004be65f6b856532bf0ddd0b8a575
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-11-11 14:15:42 +0330

    week15 E05 category horizontal list and font

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/widgets.dart';
 
 class CustomColors {
   static const Color blueIndicator = Color(0xff3B5EDF);
+  static const Color backgroundScreenColor = Color(0xffEEEEEE);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:apple_shop/constants/colors.dart';
-import 'package:apple_shop/widgets/banner_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,42 +13,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: CustomColors.backgroundScreenColor,
         body: SafeArea(
-            child: Column(
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  height: 56,
-                  width: 56,
-                  decoration: ShapeDecoration(
-                      color: Colors.red,
-                      shadows: [
-                        BoxShadow(
-                          color: Colors.red,
-                          blurRadius: 40,
-                          spreadRadius: -6,
-                          offset: Offset(0.0, 15),
-                        )
-                      ],
-                      shape: ContinuousRectangleBorder(
-                          borderRadius: BorderRadius.circular(40))),
-                ),
-                Icon(
-                  Icons.mouse,
-                  color: Colors.white,
-                  size: 32,
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text('test')
-          ],
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 44),
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: ((context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: CategoryHorizontalItemList(),
+                );
+              })),
         )),
       ),
+    );
+  }
+}
+
+class CategoryHorizontalItemList extends StatelessWidget {
+  const CategoryHorizontalItemList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              height: 56,
+              width: 56,
+              decoration: ShapeDecoration(
+                  color: Colors.red,
+                  shadows: const [
+                    BoxShadow(
+                      color: Colors.red,
+                      blurRadius: 25,
+                      spreadRadius: -12,
+                      offset: Offset(0.0, 15),
+                    )
+                  ],
+                  shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(40))),
+            ),
+            const Icon(
+              Icons.mouse,
+              color: Colors.white,
+              size: 32,
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          'همه',
+          style: TextStyle(fontFamily: 'SB', fontSize: 12),
+        )
+      ],
     );
   }
 }
