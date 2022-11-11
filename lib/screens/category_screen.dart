a2d0ea7b3f9e5d720eece67e1f4a2d9a29882435
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-11-11 18:17:37 +0330

    week15 E09 category screen

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 44, right: 44, bottom: 32),
+                child: Container(
+                  height: 46,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Image.asset('assets/images/icon_apple_blue.png'),
+                      const Expanded(
+                        child: Text(
+                          'دسته بندی',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontFamily: 'sb',
+                              fontSize: 16,
+                              color: CustomColors.blue),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 44),
+              sliver: SliverGrid(
+                delegate: SliverChildBuilderDelegate(((context, index) {
+                  return Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  );
+                })),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
