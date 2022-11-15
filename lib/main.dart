298e82d0616c65c81caa69a0deda05db808ccd7a
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-11-15 15:52:05 +0330

    week15 E12 bottomnavigationbar icons and change screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,49 +13,124 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedBottomNavigationIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: ProductListScreen(),
+        body: IndexedStack(
+          index: selectedBottomNavigationIndex,
+          children: getScreens(),
+        ),
         bottomNavigationBar: ClipRRect(
           child: BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
             child: BottomNavigationBar(
+              onTap: (int index) {
+                setState(() {
+                  selectedBottomNavigationIndex = index;
+                });
+              },
+              currentIndex: selectedBottomNavigationIndex,
               type: BottomNavigationBarType.fixed,
               backgroundColor: Colors.transparent,
               elevation: 0,
+              selectedLabelStyle: const TextStyle(
+                  fontFamily: 'sb', fontSize: 10, color: CustomColors.blue),
+              unselectedLabelStyle: const TextStyle(
+                  fontFamily: 'sb', fontSize: 10, color: Colors.black),
               items: [
                 BottomNavigationBarItem(
-                    icon: Container(
-                      child: Image.asset('assets/images/icon_home.png'),
-                      decoration: const BoxDecoration(boxShadow: [
-                        BoxShadow(
-                            color: CustomColors.blue,
-                            blurRadius: 20,
-                            spreadRadius: -7,
-                            offset: Offset(0.0, 13))
-                      ]),
+                    icon: Image.asset('assets/images/icon_profile.png'),
+                    activeIcon: Padding(
+                      padding: const EdgeInsets.only(bottom: 3),
+                      child: Container(
+                        child: Image.asset(
+                            'assets/images/icon_profile_active.png'),
+                        decoration: const BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              color: CustomColors.blue,
+                              blurRadius: 20,
+                              spreadRadius: -7,
+                              offset: Offset(0.0, 13))
+                        ]),
+                      ),
                     ),
-                    label: 'home'),
+                    label: 'حساب کاربری'),
                 BottomNavigationBarItem(
-                    icon: Image.asset('assets/images/icon_home.png'),
-                    label: 'home'),
+                    icon: Image.asset('assets/images/icon_basket.png'),
+                    activeIcon: Padding(
+                      padding: const EdgeInsets.only(bottom: 3),
+                      child: Container(
+                        child:
+                            Image.asset('assets/images/icon_basket_active.png'),
+                        decoration: const BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              color: CustomColors.blue,
+                              blurRadius: 20,
+                              spreadRadius: -7,
+                              offset: Offset(0.0, 13))
+                        ]),
+                      ),
+                    ),
+                    label: 'سبد خرید'),
                 BottomNavigationBarItem(
-                    icon: Image.asset('assets/images/icon_home.png'),
-                    label: 'home'),
+                    icon: Image.asset('assets/images/icon_category.png'),
+                    activeIcon: Padding(
+                      padding: const EdgeInsets.only(bottom: 3),
+                      child: Container(
+                        child: Image.asset(
+                            'assets/images/icon_category_active.png'),
+                        decoration: const BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              color: CustomColors.blue,
+                              blurRadius: 20,
+                              spreadRadius: -7,
+                              offset: Offset(0.0, 13))
+                        ]),
+                      ),
+                    ),
+                    label: 'دسته بندی'),
                 BottomNavigationBarItem(
-                    icon: Image.asset('assets/images/icon_home.png'),
-                    label: 'home'),
+                    icon: Image.asset('assets/images/icon_profile.png'),
+                    activeIcon: Padding(
+                      padding: const EdgeInsets.only(bottom: 3),
+                      child: Container(
+                        child: Image.asset(
+                            'assets/images/icon_profile_active.png'),
+                        decoration: const BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              color: CustomColors.blue,
+                              blurRadius: 20,
+                              spreadRadius: -7,
+                              offset: Offset(0.0, 13))
+                        ]),
+                      ),
+                    ),
+                    label: 'حساب کاربری'),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> getScreens() {
+    return <Widget>[
+      HomeScreen(),
+      CategoryScreen(),
+      ProductListScreen(),
+      CategoryScreen()
+    ];
   }
 }
