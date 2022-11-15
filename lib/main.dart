ee401df1cc09644a26a86a592ef7fe958b800561
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-11-15 19:35:58 +0330

    tempe

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:apple_shop/constants/colors.dart';
 import 'package:apple_shop/screens/category_screen.dart';
 import 'package:apple_shop/screens/home_screen.dart';
+import 'package:apple_shop/screens/product_detail_screen.dart';
 import 'package:apple_shop/screens/product_list_screen.dart';
 import 'package:apple_shop/screens/profile_screen.dart';
 import 'package:apple_shop/widgets/banner_slider.dart';
@@ -27,100 +28,97 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: IndexedStack(
-          index: selectedBottomNavigationIndex,
-          children: getScreens(),
-        ),
-        bottomNavigationBar: ClipRRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
-            child: BottomNavigationBar(
-              onTap: (int index) {
-                setState(() {
-                  selectedBottomNavigationIndex = index;
-                });
-              },
-              currentIndex: selectedBottomNavigationIndex,
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              selectedLabelStyle: const TextStyle(
-                  fontFamily: 'sb', fontSize: 10, color: CustomColors.blue),
-              unselectedLabelStyle: const TextStyle(
-                  fontFamily: 'sb', fontSize: 10, color: Colors.black),
-              items: [
-                BottomNavigationBarItem(
-                    icon: Image.asset('assets/images/icon_profile.png'),
-                    activeIcon: Padding(
-                      padding: const EdgeInsets.only(bottom: 3),
-                      child: Container(
-                        child: Image.asset(
-                            'assets/images/icon_profile_active.png'),
-                        decoration: const BoxDecoration(boxShadow: [
-                          BoxShadow(
-                              color: CustomColors.blue,
-                              blurRadius: 20,
-                              spreadRadius: -7,
-                              offset: Offset(0.0, 13))
-                        ]),
-                      ),
-                    ),
-                    label: 'حساب کاربری'),
-                BottomNavigationBarItem(
-                    icon: Image.asset('assets/images/icon_basket.png'),
-                    activeIcon: Padding(
-                      padding: const EdgeInsets.only(bottom: 3),
-                      child: Container(
-                        child:
-                            Image.asset('assets/images/icon_basket_active.png'),
-                        decoration: const BoxDecoration(boxShadow: [
-                          BoxShadow(
-                              color: CustomColors.blue,
-                              blurRadius: 20,
-                              spreadRadius: -7,
-                              offset: Offset(0.0, 13))
-                        ]),
-                      ),
-                    ),
-                    label: 'سبد خرید'),
-                BottomNavigationBarItem(
-                    icon: Image.asset('assets/images/icon_category.png'),
-                    activeIcon: Padding(
-                      padding: const EdgeInsets.only(bottom: 3),
-                      child: Container(
-                        child: Image.asset(
-                            'assets/images/icon_category_active.png'),
-                        decoration: const BoxDecoration(boxShadow: [
-                          BoxShadow(
-                              color: CustomColors.blue,
-                              blurRadius: 20,
-                              spreadRadius: -7,
-                              offset: Offset(0.0, 13))
-                        ]),
-                      ),
-                    ),
-                    label: 'دسته بندی'),
-                BottomNavigationBarItem(
-                    icon: Image.asset('assets/images/icon_home.png'),
-                    activeIcon: Padding(
-                      padding: const EdgeInsets.only(bottom: 3),
-                      child: Container(
-                        child:
-                            Image.asset('assets/images/icon_home_active.png'),
-                        decoration: const BoxDecoration(boxShadow: [
-                          BoxShadow(
-                              color: CustomColors.blue,
-                              blurRadius: 20,
-                              spreadRadius: -7,
-                              offset: Offset(0.0, 13))
-                        ]),
-                      ),
-                    ),
-                    label: 'خانه'),
-              ],
-            ),
-          ),
-        ),
+        body: ProductDetailScreen(),
+        // bottomNavigationBar: ClipRRect(
+        //   child: BackdropFilter(
+        //     filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+        //     child: BottomNavigationBar(
+        //       onTap: (int index) {
+        //         setState(() {
+        //           selectedBottomNavigationIndex = index;
+        //         });
+        //       },
+        //       currentIndex: selectedBottomNavigationIndex,
+        //       type: BottomNavigationBarType.fixed,
+        //       backgroundColor: Colors.transparent,
+        //       elevation: 0,
+        //       selectedLabelStyle: const TextStyle(
+        //           fontFamily: 'sb', fontSize: 10, color: CustomColors.blue),
+        //       unselectedLabelStyle: const TextStyle(
+        //           fontFamily: 'sb', fontSize: 10, color: Colors.black),
+        //       items: [
+        //         BottomNavigationBarItem(
+        //             icon: Image.asset('assets/images/icon_profile.png'),
+        //             activeIcon: Padding(
+        //               padding: const EdgeInsets.only(bottom: 3),
+        //               child: Container(
+        //                 child: Image.asset(
+        //                     'assets/images/icon_profile_active.png'),
+        //                 decoration: const BoxDecoration(boxShadow: [
+        //                   BoxShadow(
+        //                       color: CustomColors.blue,
+        //                       blurRadius: 20,
+        //                       spreadRadius: -7,
+        //                       offset: Offset(0.0, 13))
+        //                 ]),
+        //               ),
+        //             ),
+        //             label: 'حساب کاربری'),
+        //         BottomNavigationBarItem(
+        //             icon: Image.asset('assets/images/icon_basket.png'),
+        //             activeIcon: Padding(
+        //               padding: const EdgeInsets.only(bottom: 3),
+        //               child: Container(
+        //                 child:
+        //                     Image.asset('assets/images/icon_basket_active.png'),
+        //                 decoration: const BoxDecoration(boxShadow: [
+        //                   BoxShadow(
+        //                       color: CustomColors.blue,
+        //                       blurRadius: 20,
+        //                       spreadRadius: -7,
+        //                       offset: Offset(0.0, 13))
+        //                 ]),
+        //               ),
+        //             ),
+        //             label: 'سبد خرید'),
+        //         BottomNavigationBarItem(
+        //             icon: Image.asset('assets/images/icon_category.png'),
+        //             activeIcon: Padding(
+        //               padding: const EdgeInsets.only(bottom: 3),
+        //               child: Container(
+        //                 child: Image.asset(
+        //                     'assets/images/icon_category_active.png'),
+        //                 decoration: const BoxDecoration(boxShadow: [
+        //                   BoxShadow(
+        //                       color: CustomColors.blue,
+        //                       blurRadius: 20,
+        //                       spreadRadius: -7,
+        //                       offset: Offset(0.0, 13))
+        //                 ]),
+        //               ),
+        //             ),
+        //             label: 'دسته بندی'),
+        //         BottomNavigationBarItem(
+        //             icon: Image.asset('assets/images/icon_home.png'),
+        //             activeIcon: Padding(
+        //               padding: const EdgeInsets.only(bottom: 3),
+        //               child: Container(
+        //                 child:
+        //                     Image.asset('assets/images/icon_home_active.png'),
+        //                 decoration: const BoxDecoration(boxShadow: [
+        //                   BoxShadow(
+        //                       color: CustomColors.blue,
+        //                       blurRadius: 20,
+        //                       spreadRadius: -7,
+        //                       offset: Offset(0.0, 13))
+        //                 ]),
+        //               ),
+        //             ),
+        //             label: 'خانه'),
+        //       ],
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../constants/colors.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.backgroundScreenColor,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 44, right: 44, bottom: 32),
+                child: Container(
+                  height: 46,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Image.asset('assets/images/icon_apple_blue.png'),
+                      const Expanded(
+                        child: Text(
+                          'آیفون',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontFamily: 'sb',
+                              fontSize: 16,
+                              color: CustomColors.blue),
+                        ),
+                      ),
+                      Image.asset('assets/images/icon_back.png'),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Text(
+                  'Se 2022 آیفون',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'sb', fontSize: 16, color: Colors.black),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 44),
+                child: Container(
+                  height: 284,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 15, right: 15, top: 10),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              'assets/images/icon_star.png',
+                            ),
+                            const Text(
+                              '۴.۶',
+                              style: TextStyle(fontFamily: 'sm', fontSize: 12),
+                            ),
+                            Spacer(),
+                            Image.asset(
+                              'assets/images/iphone.png',
+                            ),
+                            Spacer(),
+                            Image.asset(
+                                'assets/images/icon_favorite_deactive.png')
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 44),
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: 10,
+                            itemBuilder: ((context, index) {
+                              return Container(
+                                height: 70,
+                                width: 70,
+                                margin: EdgeInsets.only(left: 20),
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(10)),
+                                  border: Border.all(
+                                      width: 1, color: CustomColors.gery),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(6),
+                                  child:
+                                      Image.asset('assets/images/iphone.png'),
+                                ),
+                              );
+                            }),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
