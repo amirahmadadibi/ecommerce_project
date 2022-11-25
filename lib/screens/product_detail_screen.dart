42c2807dd194be4ed8ec62419e02305d6c536040
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-11-25 23:27:59 +0330

    week15 E17 compete product detail page

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -451,10 +453,157 @@ class ProductDetailScreen extends StatelessWidget {
                   ],
                 ),
               ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20, right: 44, left: 44),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    PriceTagButton(),
+                    AddToBasketButton(),
+                  ],
+                ),
+              ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class AddToBasketButton extends StatelessWidget {
+  const AddToBasketButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.bottomCenter,
+      children: [
+        Positioned(
+          child: Container(
+            height: 60,
+            width: 140,
+            decoration: const BoxDecoration(
+                color: CustomColors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+          ),
+        ),
+        Positioned(
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(15)),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(
+                height: 53,
+                width: 160,
+                child: const Center(
+                  child: Text(
+                    'افزودن سبد خرید',
+                    style: TextStyle(
+                        fontFamily: 'sb', fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class PriceTagButton extends StatelessWidget {
+  const PriceTagButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.bottomCenter,
+      children: [
+        Positioned(
+          child: Container(
+            height: 60,
+            width: 140,
+            decoration: const BoxDecoration(
+                color: CustomColors.green,
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+          ),
+        ),
+        Positioned(
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(15)),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(
+                height: 53,
+                width: 160,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      const Text(
+                        'تومان',
+                        style: TextStyle(
+                            fontFamily: 'sm',
+                            fontSize: 12,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            '۴۹،۸۰۰،۰۰۰',
+                            style: TextStyle(
+                                fontFamily: 'sm',
+                                fontSize: 12,
+                                color: Colors.white,
+                                decoration: TextDecoration.lineThrough),
+                          ),
+                          Text(
+                            '۴۸،۸۰۰،۰۰۰',
+                            style: TextStyle(
+                              fontFamily: 'sm',
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 2, horizontal: 6),
+                          child: Text(
+                            '٪۳',
+                            style: TextStyle(
+                                fontFamily: 'sb',
+                                fontSize: 12,
+                                color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
