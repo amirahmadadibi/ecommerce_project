4e21151d066415e3acf67c33dff17426f9e5f4c6
amirahmadadibi <amirahmadadibi@gmail.com>
2023-06-04 22:37:05 +0330

    week17 E76 zarinpal payment part 2

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -11,6 +11,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:zarinpal/zarinpal.dart';
 
 class CardScreen extends StatefulWidget {
@@ -29,6 +30,7 @@ class _CardScreenState extends State<CardScreen> {
     _paymentRequest.setIsSandBox(true);
     _paymentRequest.setAmount(1000);
     _paymentRequest.setDescription('this is for test application apple shop');
+    _paymentRequest.setMerchantID('d645fba8-1b29-11ea-be59-000c295eb8fc');
     _paymentRequest.setCallbackURL('expertflutter://shop');
   }
 
@@ -109,7 +111,15 @@ class _CardScreenState extends State<CardScreen> {
                               backgroundColor: CustomColors.green,
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(15))),
-                          onPressed: () {},
+                          onPressed: () {
+                            ZarinPal().startPayment(_paymentRequest,
+                                (status, paymentGatewayUri) {
+                              if (status == 100) {
+                                launchUrl(Uri.parse(paymentGatewayUri!),
+                                    mode: LaunchMode.externalApplication);
+                              }
+                            });
+                          },
                           child: Text((state.basketFinalPrice == 0)
                               ? 'سبد خرید شما خالیه '
                               : '${state.basketFinalPrice}')),
